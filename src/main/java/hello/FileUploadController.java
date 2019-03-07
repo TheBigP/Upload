cc76f78ec5e8793cb7f6ad6bb745b6a5db2d850c
TheBig <2571019688@qq.com>
2019-03-07 15:46:25 +0800

    新增gradle版本

--- src/main/java/hello/FileUploadController.java
+++ src/main/java/hello/FileUploadController.java
@@ -8,9 +8,14 @@ import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import hello.storage.StorageFileNotFoundException;
 import hello.storage.StorageService;
 
 @Controller
@@ -40,6 +46,15 @@ public class FileUploadController {
         return "index";
     }
 
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
     @PostMapping("/")
     @ResponseBody
     public Map<String, Boolean> handleFileUpload(@RequestParam("file") MultipartFile file,
@@ -52,6 +67,11 @@ public class FileUploadController {
         Map<String, Boolean> map = new HashMap<String, Boolean>();
         map.put("success", true);
         return map;
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
     }
 
 }
--- src/main/java/hello/storage/StorageService.java
+++ src/main/java/hello/storage/StorageService.java
@@ -1,10 +1,10 @@
 package hello.storage;
 
-import org.springframework.core.io.Resource;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.nio.file.Path;
 import java.util.stream.Stream;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface StorageService {
 
